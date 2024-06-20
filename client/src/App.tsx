291b4268fd573d04d888eb054289345d9dea31bb
Darsheel Chudal <74804981+darsheelchudal@users.noreply.github.com>
2024-06-20 19:44:37 +0545

    cant update and delete

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,12 @@
 import "./App.css";
-import { usePostsQuery } from "./services/postApi";
-import { SinglePost } from "./ContactDetail";
+import {
+  usePostsQuery,
+  useSinglepostQuery,
+  useAddPostMutation,
+  useUpdatePostMutation,
+  useDeletePostMutation,
+} from "./services/postApi";
+import { Post } from "./services/postApi"; // Ensure you import the Post interface
 
 function App() {
   const { data, error, isLoading, isFetching, isSuccess } = usePostsQuery();
@@ -20,21 +26,64 @@ function App() {
           <div>
             {data?.map((post) => {
               return (
-                <>
-                  <div className="data" key={post.id}>
-                    <span>{post.title}</span>
-                    <span>
-                      <SinglePost id={post?.id.toString()} />
-                    </span>
-                  </div>
-                </>
+                <div className="data" key={post.id}>
+                  <span>{post.title}</span>
+                  <span>
+                    <SinglePost id={post.id.toString()} />
+                  </span>
+                </div>
               );
             })}
+            <div>
+              <AddPost />
+            </div>
           </div>
         )}
       </div>
     </>
   );
 }
+
+export const SinglePost = ({ id }: { id: string }) => {
+  const { data } = useSinglepostQuery(id);
+
+  return (
+    <>
+      <pre>{JSON.stringify(data)}</pre>
+    </>
+  );
+};
+
+export const AddPost = () => {
+  const [addPost] = useAddPostMutation();
+  const [updatePost] = useUpdatePostMutation();
+  const [deletePost] = useDeletePostMutation();
+
+  const initialPost: Post = { id: "1", title: "Darsheel", views: 100 };
+
+  const updatedPost: Post = { id: "1", title: "Darsheel", views: 100 };
+
+  const addHandler = async () => {
+    await addPost(initialPost);
+  };
+
+  const updateHandler = async () => {
+    await updatePost(updatedPost);
+  };
+
+  const deleteHandler = async () => {
+    await deletePost(String(initialPost.id)); // Pass the id as a string
+  };
+
+  return (
+    <>
+      <div>
+        <button onClick={addHandler}>Add</button>
+        <button onClick={updateHandler}>Update</button>
+        <button onClick={deleteHandler}>Delete</button>
+      </div>
+    </>
+  );
+};
 
 export default App;
--- client/src/services/postApi.tsx
+++ client/src/services/postApi.tsx
@@ -4,16 +4,48 @@ import { Post } from "../models/post.model";
 export const postApi = createApi({
   reducerPath: "postApi", // Unique key for the API slice reducer
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:8000/" }),
+  tagTypes: ["Post"],
   endpoints: (builder) => ({
     posts: builder.query<Post[], void>({
       query: () => "/posts",
+      providesTags: ["Post"],
     }),
     singlepost: builder.query<Post, string>({
       query: (id) => `/posts/${id}`,
+      providesTags: ["Post"],
+    }),
+    addPost: builder.mutation<void, Post>({
+      query: (post) => ({
+        url: "/posts",
+        method: "POST",
+        body: post,
+      }),
+      invalidatesTags: ["Post"],
+    }),
+    updatePost: builder.mutation<void, Post>({
+      query: (id, ...rest) => ({
+        url: `/posts/${id}`,
+        method: "PUT",
+        body: rest,
+      }),
+      invalidatesTags: ["Post"],
+    }),
+    deletePost: builder.mutation<void, string>({
+      query: (id) => ({
+        url: `/posts/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["Post"],
     }),
   }),
 });
 
-export const { usePostsQuery, useSinglepostQuery } = postApi;
+export const {
+  usePostsQuery,
+  useSinglepostQuery,
+  useAddPostMutation,
+  useUpdatePostMutation,
+  useDeletePostMutation,
+} = postApi;
 
 export default postApi;
