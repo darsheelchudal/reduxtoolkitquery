0209c4aaf984e5c18223d3c668ff7d3242a5cc4c
Darsheel Chudal <74804981+darsheelchudal@users.noreply.github.com>
2024-06-20 06:29:12 +0545

    final commit

--- client/src/services/postApi.tsx
+++ client/src/services/postApi.tsx
@@ -1,8 +1,16 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { Post } from "../models/post.model";
 
 const postApi = createApi({
+  reducerPath: "postApi", // Unique key for the API slice reducer
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:8000/" }),
   endpoints: (builder) => ({
-    posts: builder.query<>(),
+    posts: builder.query<Post[], void>({
+      query: () => "/posts",
+    }),
   }),
 });
+
+export const { usePostsQuery } = postApi; // Correctly export the generated hook
+
+export default postApi;
